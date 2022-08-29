3b45dd86706db9aca0f4a6d9891570cbaa72b8d0
漢Karma #TLM漢 <94296856+underagent@users.noreply.github.com>
2022-08-29 19:47:28 +0200

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,25 @@
+#imports:
+import ast
+import inspect
+import re
+import discord #lel
+
+def source(o):
+    s = inspect.getsource(o).split("\n")
+    indent = len(s[0]) - len(s[0].lstrip())
+    return "\n".join(i[indent:] for i in s)
+
+
+source_ = source(discord.gateway.DiscordWebSocket.identify)
+patched = re.sub(
+    r'([\'"]\$browser[\'"]:\s?[\'"]).+([\'"])',
+    r"\1Discord Android\2",
+    source_
+)
+
+loc = {}
+exec(compile(ast.parse(patched), "<string>", "exec"), discord.gateway.__dict__, loc)
+
+discord.gateway.DiscordWebSocket.identify = loc["identify"]
+
+#rest of your nonsense
